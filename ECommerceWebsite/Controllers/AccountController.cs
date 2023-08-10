f85869ed80977e457f45621f7f8156db18b8fdd7
Dipesh Wagle <deepacewagle@gmail.com>
2023-08-10 14:58:26 +0545

    New changes for Login and Register

--- ECommerceWebsite/Controllers/AccountController.cs
+++ ECommerceWebsite/Controllers/AccountController.cs
@@ -2,13 +2,12 @@
 using ECommerceWebsite.Models;
 using ECommerceWebsite.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace ECommerceWebsite.Controllers
 {
-    [Route("[controller]")]
-
     public class AccountController : Controller
     {
         private readonly IUserRepository _userRepository;
@@ -17,31 +16,78 @@ namespace ECommerceWebsite.Controllers
             _userRepository = userRepository;
         }
 
-        [HttpGet("register")]
+        [HttpGet]
        
         public IActionResult Register()
         
+        
         {
-            return View();
+            RegisterDTO model = new();
+            ViewBag.Message = TempData["Message"];
+            ViewBag.Type = TempData["Type"];
+            TempData.Remove("Message");
+            TempData.Remove("Type");
+            return View(model);
         }
 
-        [HttpPost("register")]
-        public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDto)
+        [HttpPost]
+        public async Task<ActionResult<UserDTO>> Register(RegisterDTO model)
         {
-            if (await _userRepository.UserExists(registerDto.Username))
+            if (await _userRepository.PhoneNumberExists(model.PhoneNumber))
             {
-                return BadRequest("Username is already taken");
+                TempData["Message"] = "Username is already taken";
+                TempData["Type"] = "error";
+                return RedirectToAction("Register");
+
             }
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
-                UserName = registerDto.Username.ToLower(),
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-                PasswordSalt = hmac.Key
+                PhoneNumber = model.PhoneNumber,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(model.Password)),
+                PasswordSalt = hmac.Key,
+                Email = "Admin@123",
+                Username = "Admin"
             };
             await _userRepository.RegisterUser(user);
-            return View();
+            TempData["Message"] = "Registered successfully";
+            TempData["Type"] = "success";
+            return RedirectToAction("Register");
         }
 
+        public IActionResult Login()
+        
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginDTO loginDTO)
+        {
+            var user = await _userRepository.GetUserByPhoneNumberAsync(loginDTO.PhoneNumber);
+            if (user == null)
+            {
+                TempData["Message"] = "Invalid Phone Number";
+
+            }
+
+            if (user == null)
+            {
+                TempData["Message"] = "Invalid Username";
+            } 
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+            for (var i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                {
+                    TempData["Message"] = "Incorrect Password";
+                }
+            }
+
+            TempData["Message"] = "Logged In successfully";
+            TempData["Type"] = "success";
+            return RedirectToAction("Privacy","Home");
+        }
     }
 }
+
